eab4674c536507c747af4562e75f746a35c9fe7f
libredesarrollo <desarrollolibre.net@gmail.com>
2022-03-12 14:03:31 -0400

    sacntum

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use Illuminate\Routing\RouteGroup;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('category', CategoryController::class)->except(["create", "edit"]);
-Route::get('category/{category}/posts',[CategoryController::class, 'posts']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('category', CategoryController::class)->except(["create", "edit"]);
+    Route::get('category/{category}/posts', [CategoryController::class, 'posts']);
+});
+
+
 
 
 
 Route::resource('post', PostController::class)->except(["create", "edit"]);
-Route::get('post/slug/{post:slug}',[PostController::class, 'slug']);
+Route::get('post/slug/{post:slug}', [PostController::class, 'slug']);
